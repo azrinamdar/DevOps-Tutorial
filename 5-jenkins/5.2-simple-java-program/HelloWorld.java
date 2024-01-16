f7cdce1f02e31c680fdecc8b461c416cb25c6e57
Syed Mohammad Azharuddin Inamdar <155865607+azrinamdar@users.noreply.github.com>
2024-01-16 13:36:25 +0530

    Update HelloWorld.java

--- 5-jenkins/5.2-simple-java-program/HelloWorld.java
+++ 5-jenkins/5.2-simple-java-program/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
 	System.out.println("Hello, World From Varun Manik on date MaJan 13 2024");
+	System.out.println("This line is added by Azhar");
   }
 }
